0dc2480f0af92496dc92bde1f38d6e2d89645177
Zhandaulet <73915954+zhandaule@users.noreply.github.com>
2023-11-14 19:09:53 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Задание выполнено</title>
+    <style>
+        .animated-text {
+            animation: fadeInOut 2s infinite;
+        }
+        @keyframes fadeInOut {
+            0% { opacity: 0; }
+            50% { opacity: 1; }
+            100% { opacity: 0; }
+        }
+    </style>
+</head>
+<body>
+
+<div class="animated-text">
+    Я выполнил задание
+</div>
+
+</body>
+</html>
