50161f3907e9ae80a1b1b732020b6580c652c97c
Zhandaulet <73915954+zhandaule@users.noreply.github.com>
2023-11-14 19:40:19 +0600

    index.php

--- index.php
+++ index.php
@@ -1,23 +1,52 @@
+<!-- index.php or index.html -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Задание выполнено</title>
-    <style>
-        .animated-text {
-            animation: fadeInOut 2s infinite;
-        }
-        @keyframes fadeInOut {
-            0% { opacity: 0; }
-            50% { opacity: 1; }
-            100% { opacity: 0; }
-        }
-    </style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Animated Page</title>
+<style>
+  body, html {
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: grey;
+  }
+  #animatedText {
+    font-size: 2em;
+    /* Additional styling for the text */
+  }
+  #backgroundImage {
+    position: absolute;
+    left: 0;
+    right: 0;
+    animation: appear-disappear 10s infinite;
+    /* Additional styling for the background image */
+  }
+  @keyframes appear-disappear {
+    0%, 100% {
+      opacity: 0;
+      transform: translateX(-100%);
+    }
+    25%, 75% {
+      opacity: 1;
+      transform: translateX(0);
+    }
+    50% {
+      opacity: 1;
+      transform: translateX(100%);
+    }
+  }
+</style>
 </head>
 <body>
-
-<div class="animated-text">
-    Я выполнил задание
-</div>
-
+<div id="animatedText">Сделанно</div>
+<img id="backgroundImage" src="lesson/bg.jpg" alt="Animated Background">
+<script>
+  // JavaScript to change the image and text dynamically if needed
+</script>
 </body>
 </html>
